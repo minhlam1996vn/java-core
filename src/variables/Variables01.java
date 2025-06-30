067bb689926762d2e673e17d7b00d744935b1738
Minh Lam <minhlam1996vn@gmail.com>
2025-06-30 15:22:05 +0700

    variables/Variable01

---
+++ src/variables/Variables01.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class Variables01 {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 9;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
+
+// 15
