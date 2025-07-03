3fb0d2c11615b148867f10173cda9f4e95d8ee54
Minh Lam <minhlam1996vn@gmail.com>
2025-07-03 14:55:23 +0700

    ArrayList

--- src/arrayList/ArrayList01.java
+++ src/arrayList/ArrayList01.java
@@ -1,10 +1,20 @@
 package arrayList;
 
+//    https://www.w3schools.com/java/java_data_types.asp
+//    https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+//    https://stackoverflow.com/questions/29284402/why-do-reference-data-types-point
+//    https://stackoverflow.com/questions/2721546/why-dont-java-generics-support-primitive-types
+//    https://docs.oracle.com/javase/tutorial/java/data/autoboxing.html
+//    https://www.w3schools.com/java/java_arraylist.asp
+//    https://docs.oracle.com/javase/tutorial/java/generics/index.html
+//    https://docs.oracle.com/javase/tutorial/java/generics/types.html
+
 import java.util.ArrayList;
 
 public class ArrayList01 {
     public static void main(String[] args) {
-        ArrayList<String> a1 = new ArrayList<String>(); // generic
+//        ArrayList<String> a1 = new ArrayList<String>(); // generic
+        ArrayList<String> a1 = new ArrayList<>(); // Java Diamond (tự động đoán type từ java version 7)
         a1.add("Minh Lam 01");
         a1.add("Minh Lam 02");
         a1.add("Minh Lam 03");
